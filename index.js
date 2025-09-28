90f5106459a4f6f1e47a55bc536209ced1c2752e
krak <notreallykrak@gmail.com>
2025-09-28 17:15:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, Collection } = require("discord.js");
+const { createDatabase } = require("whisky.db");
+const db = new createDatabase();
+
+const client = new Client({
+    intents: 32767,
+});
+module.exports = client;
+
+client.commands = new Collection();
+client.slashCommands = new Collection();
+client.config = require("./config.json");
+
+require("./handler")(client);
+
+client.login(client.config.token);
+
+// developed by youtube.com/@djsturkiye // Whisky Lorean
